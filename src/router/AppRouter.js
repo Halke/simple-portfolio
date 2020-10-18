a3b20d3a91685adf5f6c85d8b9761af17832ab4f
Halke <dinohalilovichalke@gmail.com>
2020-10-18 21:11:09 +0200

    Added default css file

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import "../css/App.css";
 
 function AppRouter() {
     return (
