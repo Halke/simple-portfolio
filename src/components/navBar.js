09d9dcefe054d736c5bac18cb6d77e343410b51f
Halke <dinohalilovichalke@gmail.com>
2020-10-18 21:10:49 +0200

    Repaired errors

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import {
     BrowserView,
@@ -8,27 +9,29 @@ import {
 // The component that renders navbar optimized for
 // mobile phones
 function RenderMobileNavbar() {
+    return (
     <ul>
-        <li><a href="#"><img src={process.env.PUBLIC_URL + "/icons/whiteHome.png"} alt="Home option"/></a></li>
-        <li><a href="#"><img src={process.env.PUBLIC_URL + "/icons/projectsWhite.png"} alt="Projects option"/></a></li>
-        <li><a href="#"><img src={process.env.PUBLIC_URL + "/icons/profileWhite.png"} alt="About option"/></a></li>
-    </ul>
+        <li><a href="#home"><img src={process.env.PUBLIC_URL + "/icons/whiteHome.png"} alt="Home option"/></a></li>
+        <li><a href="#projects"><img src={process.env.PUBLIC_URL + "/icons/projectsWhite.png"} alt="Projects option"/></a></li>
+        <li><a href="#about"><img src={process.env.PUBLIC_URL + "/icons/profileWhite.png"} alt="About option"/></a></li>
+    </ul>);
 }
 
 // The component that renders navbar optimized for
 // PCs and laptops
 function RenderBrowserNavbar() {
+    return (
     <ul>
-        <li>HOME</li>
+        <li><a href="#home">HOME</a></li>
         <li>&#9787;</li>
-        <li>PROJECTS</li>
+        <li><a href="#projects">PROJECTS</a></li>
         <li>&#9787;</li>
-        <li>ABOUT</li>
-    </ul>
+        <li><a href="#about">ABOUT</a></li>
+    </ul>);
 }
 
 
-function navBar() {
+function NavBar() {
 
     return (
         <div className="navbar">
@@ -42,4 +45,4 @@ function navBar() {
     )
 }
 
-export default navBar
+export default NavBar;
