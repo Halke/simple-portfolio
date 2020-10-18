084de2b7f8b71996032cd67b308ea88df67ccd74
Halke <dinohalilovichalke@gmail.com>
2020-10-18 20:35:29 +0200

    Created Navbar structure for browser and mobile

---
+++ src/components/navBar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {
+    BrowserView,
+    MobileView,
+    isMobile
+  } from "react-device-detect";
+
+
+// The component that renders navbar optimized for
+// mobile phones
+function RenderMobileNavbar() {
+    <ul>
+        <li><a href="#"><img src={process.env.PUBLIC_URL + "/icons/whiteHome.png"} alt="Home option"/></a></li>
+        <li><a href="#"><img src={process.env.PUBLIC_URL + "/icons/projectsWhite.png"} alt="Projects option"/></a></li>
+        <li><a href="#"><img src={process.env.PUBLIC_URL + "/icons/profileWhite.png"} alt="About option"/></a></li>
+    </ul>
+}
+
+// The component that renders navbar optimized for
+// PCs and laptops
+function RenderBrowserNavbar() {
+    <ul>
+        <li>HOME</li>
+        <li>&#9787;</li>
+        <li>PROJECTS</li>
+        <li>&#9787;</li>
+        <li>ABOUT</li>
+    </ul>
+}
+
+
+function navBar() {
+
+    return (
+        <div className="navbar">
+            <BrowserView viewClassName="navbar__browser">
+                <RenderBrowserNavbar />
+            </BrowserView>
+            <MobileView viewClassName="navbar__mobile-phone">
+                <RenderMobileNavbar />
+            </MobileView>
+        </div>
+    )
+}
+
+export default navBar
