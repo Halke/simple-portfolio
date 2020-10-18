730ec69ee891b65dfced44108c8682ac210f120d
Halke <dinohalilovichalke@gmail.com>
2020-10-18 21:57:23 +0200

    Updated classNames

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,16 +4,17 @@ import {
     BrowserView,
     MobileView
   } from "react-device-detect";
+  import "../css/navbar.css";
 
 
 // The component that renders navbar optimized for
 // mobile phones
 function RenderMobileNavbar() {
     return (
-    <ul className="navbar__options-container">
-        <li className="navbar__option"><a href="#home"><img src={process.env.PUBLIC_URL + "/icons/whiteHome.png"} alt="Home option"/></a></li>
-        <li className="navbar__option"><a href="#projects"><img src={process.env.PUBLIC_URL + "/icons/projectsWhite.png"} alt="Projects option"/></a></li>
-        <li className="navbar__option"><a href="#about"><img src={process.env.PUBLIC_URL + "/icons/profileWhite.png"} alt="About option"/></a></li>
+    <ul className="navbar__options-container-phone">
+        <li className="navbar__option-phone"><a href="#home"><img src={process.env.PUBLIC_URL + "/icons/whiteHome.png"} alt="Home option"/></a></li>
+        <li className="navbar__option-phone"><a href="#projects"><img src={process.env.PUBLIC_URL + "/icons/projectsWhite.png"} alt="Projects option"/></a></li>
+        <li className="navbar__option-phone"><a href="#about"><img src={process.env.PUBLIC_URL + "/icons/profileWhite.png"} alt="About option"/></a></li>
     </ul>);
 }
 
@@ -21,12 +22,12 @@ function RenderMobileNavbar() {
 // PCs and laptops
 function RenderBrowserNavbar() {
     return (
-    <ul className="navbar__options-container">
-        <li className="navbar__option"><a href="#home">HOME</a></li>
-        <li>&#9787;</li>
-        <li className="navbar__option"><a href="#projects">PROJECTS</a></li>
-        <li>&#9787;</li>
-        <li className="navbar__option"><a href="#about">ABOUT</a></li>
+    <ul className="navbar__options-container-browser">
+        <li className="navbar__option-browser"><a href="#home">HOME</a></li>
+        <li>&#9883;</li>
+        <li className="navbar__option-browser"><a href="#projects">PROJECTS</a></li>
+        <li>&#9883;</li>
+        <li className="navbar__option-browser"><a href="#about">ABOUT</a></li>
     </ul>);
 }
 
