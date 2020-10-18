e53fc0f69079bbe2c2872ad7c4b457df62edf672
Halke <dinohalilovichalke@gmail.com>
2020-10-18 22:00:11 +0200

    Created Heading section

---
+++ src/components/headerSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/headerSection.css";
+
+function HeaderSection() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HeaderSection;
