7ae3c64db94b1c26c0c2b49da3baead9a8a02f7c
Halke <dinohalilovichalke@gmail.com>
2020-10-18 21:57:43 +0200

    Component for pages on PCs and laptops

---
+++ src/components/FullPage/BrowserPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "../navBar";
+
+function BrowserPage() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default BrowserPage
