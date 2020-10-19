713ea5b872e713ca46b021750aa7740651b85281
Halke <dinohalilovichalke@gmail.com>
2020-10-19 13:39:08 +0200

    Added newly created components

--- src/components/FullPage/BrowserPage.js
+++ src/components/FullPage/BrowserPage.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import Navbar from "../navBar";
 import HeaderSection from "../headerSection";
+import DescriptiveSection from "../descriptiveSection";
+import "../../css/browserPage.css";
 
 function BrowserPage() {
     return (
         <div>
             <Navbar />
-            <HeaderSection />
+            <HeaderSection id="home" />
+            <div className="browser-page__content">
+                <div id="about" className="browser-page__about">
+                    <DescriptiveSection
+                        className="browser-page__about-personal-info" 
+                        heading="Personal info"
+                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis luctus lacus, et pharetra nisl tempor at. 
+                                Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. Morbi consectetur est quis magna lobortis tincidunt. 
+                                Nulla facilisi. Quisque iaculis justo ut arcu auctor molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                        fontColor="#a8dadc"
+                    />
+                    <div className="browser-page__about-profile-photo">
+                        <img className="browser-page__about-profile-photo-circle" src="https://via.placeholder.com/200" alt="Avatar"/>
+                    </div>
+                    <DescriptiveSection
+                        className="browser-page__about-my-interest" 
+                        heading="What am I interested in?"
+                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis luctus lacus, et pharetra nisl tempor at. Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. Morbi consectetur est quis magna lobortis tincidunt. Nulla facilisi. Quisque iaculis justo ut arcu auctor molestie."
+                        fontColor="#f1faee"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
