e48ed43aa252ea74a567ed45aca197680219e181
Halke <dinohalilovichalke@gmail.com>
2020-10-19 10:35:41 +0200

    Modified headerSection

--- src/components/headerSection.js
+++ src/components/headerSection.js
@@ -1,34 +1,25 @@
 import React from 'react'
-import {BrowserView, MobileView, isMobile} from "react-device-detect";
+import {BrowserView, MobileView} from "react-device-detect";
 import "../css/headerSection.css";
+
+
 
 
 function HeaderSection() {
 
-    // const styles = () => {
-    //     if(isMobile){
-    //         return {
-    //             backgroundImage: `${process.env.PUBLIC_URL}/images/mobile_home_bg.jpg` 
-    //         }
-    //     }else{
-    //         return {
-    //             backgroundImage: `${process.env.PUBLIC_URL}/images/home_bg.jpg` 
-    //         }
-    //     }
-    // }
-
-    // const styles = isMobile ? {background: `${process.env.PUBLIC_URL}/images/mobile_home_bg.jpg`} : {background: `${process.env.PUBLIC_URL}/images/home_bg.jpg`}
-
     return (
         <div className="header">
+            {/* This part will be rendered when user is using PC or laptop */}
             <BrowserView viewClassName="header__browser">
-                <div className="header__content">
+                <div className="header__content-browser">
                     <h2 className="header__content-heading">HELLO! I AM</h2>
                     <img className="header__name-image" src={`${process.env.PUBLIC_URL}/images/DINO_ime_v2.png`} alt="My name" />
                 </div>
-                </BrowserView>
-            <MobileView className="header__phone">
-                <div className="header__content">
+            </BrowserView>
+
+            {/* This part will be rendered when user is using mobile phone */}
+            <MobileView viewClassName="header__phone">
+                <div className="header__content-phone">
                     <h2 className="header__content-heading">HELLO! I AM</h2>
                     <img className="header__name-image" src={`${process.env.PUBLIC_URL}/images/DINO_ime_v2.png`} alt="My name" />
                 </div>
