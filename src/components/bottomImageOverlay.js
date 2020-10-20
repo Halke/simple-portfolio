3c605e416e4def4a4b9803d38e1cc516b61a6109
Halke <dinohalilovichalke@gmail.com>
2020-10-20 23:55:45 +0200

    Finished image hover effect components

--- src/components/bottomImageOverlay.js
+++ src/components/bottomImageOverlay.js
@@ -3,15 +3,13 @@ import "../css/bottomImageOverlay.css";
 
 function BottomImageOverlay({imageUrl}) {
     return (
-        <div className="bottom-image-overlay__container">
-            <img className="bottom-image-overlay__image" src={imageUrl} alt="Underneath"/>
-            <div className="bottom-image-overlay_overlay">
-                <div className="bottom-image-overlay__text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nunc convallis luctus lacus, et pharetra nisl tempor at. 
-                    Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. 
-                    Morbi consectetur est quis magna lobortis tincidunt. Nulla facilisi. 
-                    Quisque iaculis justo ut arcu auctor molestie.
+        <div className="bottom-overlay__container">
+            <img src={imageUrl} alt="Avatar" className="bottom-overlay__image" />
+            <div className="bottom-overlay__overlay">
+                <div className="text">
+                    Creating backend in accordance with recent programming standards and practices.
+                    This will allow anyone with understanding of backend technologies to updated 
+                    existing backend easily.
                 </div>
             </div>
         </div>
--- src/components/topImageOverlay.js
+++ src/components/topImageOverlay.js
@@ -3,15 +3,11 @@ import "../css/topImageOverlay.css";
 
 function TopImageOverlay({imageUrl}) {
     return (
-        <div className="top-image-overlay__container">
-            <img className="top-image-overlay__image" src={imageUrl} alt="Underneath"/>
-            <div className="top-image-overlay_overlay">
-                <div className="top-image-overlay__text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nunc convallis luctus lacus, et pharetra nisl tempor at. 
-                    Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. 
-                    Morbi consectetur est quis magna lobortis tincidunt. Nulla facilisi. 
-                    Quisque iaculis justo ut arcu auctor molestie.
+        <div className="container">
+            <img src={imageUrl} alt="Avatar" className="image" />
+            <div className="overlay">
+                <div className="text">
+                    Making modern Websites and Web Apps that will satisfy you users. 
                 </div>
             </div>
         </div>
