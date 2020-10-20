61ada3f80925125c5d914836145d5e534469b42b
Halke <dinohalilovichalke@gmail.com>
2020-10-20 15:34:12 +0200

    Added components for photo hover effects

---
+++ src/components/bottomImageOverlay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/bottomImageOverlay.css";
+
+function BottomImageOverlay({imageUrl}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BottomImageOverlay;
---
+++ src/components/topImageOverlay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/topImageOverlay.css";
+
+function TopImageOverlay({imageUrl}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TopImageOverlay
