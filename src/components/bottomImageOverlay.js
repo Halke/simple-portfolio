97fdc7fb33864d21052fed85923298f4bc59ae51
Halke <dinohalilovichalke@gmail.com>
2020-10-20 16:06:12 +0200

    Added structure to components

--- src/components/bottomImageOverlay.js
+++ src/components/bottomImageOverlay.js
@@ -3,8 +3,17 @@ import "../css/bottomImageOverlay.css";
 
 function BottomImageOverlay({imageUrl}) {
     return (
-        <div>
-            
+        <div className="bottom-image-overlay__container">
+            <img className="bottom-image-overlay__image" src={imageUrl} alt="Underneath"/>
+            <div className="bottom-image-overlay_overlay">
+                <div className="bottom-image-overlay__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nunc convallis luctus lacus, et pharetra nisl tempor at. 
+                    Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. 
+                    Morbi consectetur est quis magna lobortis tincidunt. Nulla facilisi. 
+                    Quisque iaculis justo ut arcu auctor molestie.
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/topImageOverlay.js
+++ src/components/topImageOverlay.js
@@ -3,8 +3,17 @@ import "../css/topImageOverlay.css";
 
 function TopImageOverlay({imageUrl}) {
     return (
-        <div>
-            
+        <div className="top-image-overlay__container">
+            <img className="top-image-overlay__image" src={imageUrl} alt="Underneath"/>
+            <div className="top-image-overlay_overlay">
+                <div className="top-image-overlay__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nunc convallis luctus lacus, et pharetra nisl tempor at. 
+                    Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. 
+                    Morbi consectetur est quis magna lobortis tincidunt. Nulla facilisi. 
+                    Quisque iaculis justo ut arcu auctor molestie.
+                </div>
+            </div>
         </div>
     )
 }
