480fea2b57b227d6bfeb1637e79548a87c902bfa
Halke <dinohalilovichalke@gmail.com>
2020-10-21 17:26:48 +0200

    Created starting structure for skills section

---
+++ src/components/skillsSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../css/skillsSection.css"
+
+function SkillsSection() {
+    return (
+        <div className="skills-section">
+            <div className="skills-section__programming-skills">
+
+            </div>
+            <div className="skills-section__other-skills">
+
+            </div>
+        </div>
+    )
+}
+
+export default SkillsSection
