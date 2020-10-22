2972777ef0af13c15900bc783104b8f77e0a3c95
Halke <dinohalilovichalke@gmail.com>
2020-10-22 13:40:14 +0200

    Added send mail button

--- src/components/sendMail.js
+++ src/components/sendMail.js
@@ -3,8 +3,11 @@ import "../css/sendMail.css";
 
 function SendMail() {
     return (
-        <div className="sned-mail">
-            
+        <div className="send-mail">
+            <h1>WANT TO COLLABORATE?</h1>
+            <button className="send-mail__open-form-btn">
+                SEND ME A MAIL <span><img className="send-mail__email-icon" src={`${process.env.PUBLIC_URL}/icons/email.png`} alt="Letter icon"/></span>
+            </button>
         </div>
     )
 }
