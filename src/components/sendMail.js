d115cf9838346eb8ef4022a2fca5820bb95d1505
Halke <dinohalilovichalke@gmail.com>
2020-10-22 14:32:45 +0200

    Completed component structure

--- src/components/sendMail.js
+++ src/components/sendMail.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../css/sendMail.css";
 
 function SendMail() {
+
+    const [showForm, setShowForm] = useState(false);
+
     return (
         <div className="send-mail">
-            <h1>WANT TO COLLABORATE?</h1>
-            <button className="send-mail__open-form-btn">
-                SEND ME A MAIL <span><img className="send-mail__email-icon" src={`${process.env.PUBLIC_URL}/icons/email.png`} alt="Letter icon"/></span>
-            </button>
+            <h1 className="send-mail__heading">WANT TO COLLABORATE?</h1>
+            <div className="send-mail__mail-form">
+                <button className="send-mail__open-form-btn" onClick={() => setShowForm(!showForm)}>
+                    SEND ME A MAIL 
+                    <span className="send-mail__arrow-icon-container">
+                        <img 
+                            className="send-mail__arrow-icon" 
+                            src={showForm === false ? 
+                                `${process.env.PUBLIC_URL}/icons/down-arrow.png` 
+                                : 
+                                `${process.env.PUBLIC_URL}/icons/up-arrow.png`} 
+                            alt="Letter icon"
+                        />
+                    </span>
+                </button>
+                {}
+            </div>
         </div>
     )
 }
