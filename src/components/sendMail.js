3e8635fbcb7b8600d218407ed668bc1226dd012f
Halke <dinohalilovichalke@gmail.com>
2020-10-22 13:35:24 +0200

    Created component and style file

---
+++ src/components/sendMail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/sendMail.css";
+
+function SendMail() {
+    return (
+        <div className="sned-mail">
+            
+        </div>
+    )
+}
+
+export default SendMail;
