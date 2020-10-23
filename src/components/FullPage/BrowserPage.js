82122ba9c0eee52f8150a5d6a470c1c9e6006d76
Halke <dinohalilovichalke@gmail.com>
2020-10-23 18:37:22 +0200

    Edited showed profile image

--- src/components/FullPage/BrowserPage.js
+++ src/components/FullPage/BrowserPage.js
@@ -20,20 +20,23 @@ function BrowserPage() {
                     <DescriptiveSection
                         className="browser-page__about-personal-info" 
                         heading="Personal info"
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis luctus lacus, et pharetra nisl tempor at. 
-                                Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. Morbi consectetur est quis magna lobortis tincidunt. 
-                                Nulla facilisi. Quisque iaculis justo ut arcu auctor molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                        content="My name is Dino Halilovic and I am 21 year old. I am the student of Software Engineering at the University of Zenica.
+                                My love for programming comes from High School when my friend introduced me few articles about programming in Python 
+                                and whole story begins there. Today, after 6 years of coding I can still feel the same love as it was in the beginning.
+                                I am hardworking and passionate programmer which will dedicate whole his work and free time to the task that has to be 
+                                done."
                         fontColor="#a8dadc"
                     />
                     <div className="browser-page__about-profile-photo">
-                        <img className="browser-page__about-profile-photo-circle" src="https://via.placeholder.com/200" alt="Avatar"/>
+                        <img className="browser-page__about-profile-photo-circle" src={`${process.env.PUBLIC_URL}/images/ProfileImg.jpg`} alt="Avatar"/>
                     </div>
                     <DescriptiveSection
                         className="browser-page__about-my-interest" 
                         heading="What am I interested in?"
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis luctus lacus, et pharetra nisl tempor at. Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor. Morbi consectetur est quis magna lobortis tincidunt. Nulla facilisi. Quisque iaculis justo ut arcu auctor molestie."
+                        content="I'm interested in Web Development. I am tending to become the programmer which can accomplish any frontend or backend
+                                task. Below is the section where the most of my skills, which are important for a Web Development, are listed and the list
+                                isn't final. It can be changed, easily. I am eager to learn and spread my knowledge. Aside from Web Development, I am very interested
+                                in Cyber Security and also in Machine Learning. But, that's the topic for different occasion."
                         fontColor="#f1faee"
                     />
                 </div>
