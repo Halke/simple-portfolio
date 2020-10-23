c777c4823c56a4725b893f867c761feb12db73a6
Halke <dinohalilovichalke@gmail.com>
2020-10-23 20:04:57 +0200

    Updates

---
+++ precache-manifest.19bb9971e72e7bafca1b5f2d96bfb8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16c8e201712f94d495f86d8af7ea2ee",
+    "url": "/simple-portfolio/index.html"
+  },
+  {
+    "revision": "f6696f6a860f78207690",
+    "url": "/simple-portfolio/static/css/main.f81f3614.chunk.css"
+  },
+  {
+    "revision": "4fcfb280faccb47fc9e8",
+    "url": "/simple-portfolio/static/js/2.59e1b96b.chunk.js"
+  },
+  {
+    "revision": "177f179dbee68262dea2e12f4e434276",
+    "url": "/simple-portfolio/static/js/2.59e1b96b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6696f6a860f78207690",
+    "url": "/simple-portfolio/static/js/main.f92a0056.chunk.js"
+  },
+  {
+    "revision": "46744b9a27955b8c6a11",
+    "url": "/simple-portfolio/static/js/runtime-main.f3562085.js"
+  }
+]);
