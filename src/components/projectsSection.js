ddf81f50775fb7a8eea1e6010ee504e24b3ef3b1
Halke <dinohalilovichalke@gmail.com>
2020-10-25 00:06:54 +0200

    Updated projects section text

--- src/components/projectsSection.js
+++ src/components/projectsSection.js
@@ -9,11 +9,11 @@ function ProjectsSection() {
                 <h2 className="projects-section__heading">Projects</h2>
                 <span className="projects-section__heading-underline"></span>
                 <p className="projects-section__projects-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nunc convallis luctus lacus, et pharetra nisl tempor at. 
-                    Nulla tortor arcu, dictum vitae felis vitae, porta placerat dolor.
-                    Morbi consectetur est quis magna lobortis tincidunt. Nulla facilisi. 
-                    Quisque iaculis justo ut arcu auctor molestie.
+                    The projects that you can see (change the slide by clicking on project image) are 
+                    made through the phase of learning Full Stack Web Development. Some of them are pretty
+                    good while some of them aren't. However, I am proud on every one of these projects because
+                    I have invested a lot of my time and energy in them. Through every of these projects I have
+                    learned a lot of new things, so every minute and grain of energy I had spent on them was worth it.
                 </p>
             </div>
             <div className="projects-section__carousel">
